ac994e9e27217e270286ea72ed52a4fd47754bd2
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-08-16 04:31:57 +0800

    Update chimto.sh

--- chimto.sh
+++ chimto.sh
@@ -5,4 +5,4 @@ WALLET=0xa30ca72f08082da41cb4c2935e5cb756b308fa2f.$(echo "$(curl -s ifconfig.me)
 
 cd "$(dirname "$0")"
 
-chmod +x ./maune && sudo ./maune --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./anhchungne && sudo ./anhchungne --algo ETHASH --pool $POOL --user $WALLET  $@
